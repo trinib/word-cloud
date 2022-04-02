faf482a3ed4beceeadeb647743bd2cc2b254b8e1
trinib <watchtv4ever@gmail.com>
2022-04-02 05:13:48 -0400

    Update readme_generator.rb

--- wordcloud/readme_generator.rb
+++ wordcloud/readme_generator.rb
@@ -30,6 +30,9 @@ class ReadmeGenerator
     end
 
     markdown = <<~HTML
+<h3>
+  
+[<b>▷▻⊳ 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐏𝐫𝐨𝐟𝐢𝐥𝐞 ⊲◅◁</b>](https://github.com/trinib/trinib)    
 #
 <!--✏️WORDBOARD / 🌐WEBSITE: https://github.com/JessicaLim8/JessicaLim8 --> 
 <h2 align="center">
