4b07915038cefe3010a49b5bcb6cb842b8d958e5
trinib <watchtv4ever@gmail.com>
2022-08-22 11:48:46 -0400

    Add files via upload

---
+++ wordcloud/readme_generator.rb
@@ -0,0 +1,77 @@
+# require_relative "./cloud_types"
+
+class ReadmeGenerator
+  WORD_CLOUD_URL = 'https://raw.githubusercontent.com/trinib/word-cloud/main/wordcloud/wordcloud.png'
+  ADDWORD = 'add'
+  SHUFFLECLOUD = 'shuffle'
+  INITIAL_COUNT = 0
+  USER = "trinib"
+
+  def initialize(octokit:)
+    @octokit = octokit
+  end
+
+  def generate
+    participants = Hash.new(0)
+    current_contributors = Hash.new(0)
+    current_words_added = INITIAL_COUNT
+    total_clouds = CloudTypes::CLOUDLABELS.length
+    total_words_added = INITIAL_COUNT * total_clouds
+
+    octokit.issues.each do |issue|
+      participants[issue.user.login] += 1
+      if issue.title.split('|')[1] != SHUFFLECLOUD && issue.labels.any? { |label| CloudTypes::CLOUDLABELS.include?(label.name) }
+        total_words_added += 1
+        if issue.labels.any? { |label| label.name == CloudTypes::CLOUDLABELS.last }
+          current_words_added += 1
+          current_contributors[issue.user.login] += 1
+        end
+      end
+    end
+
+    markdown = <<~HTML
+<h3>
+  
+[<b>▷▻⊳ 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐏𝐫𝐨𝐟𝐢𝐥𝐞 ⊲◅◁</b>](https://github.com/trinib/trinib)    
+#
+<!--✏️WORDBOARD / 🌐WEBSITE: https://github.com/JessicaLim8/JessicaLim8 --> 
+<h2 align="center">
+Join the Word Cloud Board :cloud: :pencil2:
+
+### :thought_balloon: [Add your name](https://github.com/trinib/word-cloud/issues/new?template=addword.md&title=wordcloud%7Cadd%7C%3CINSERT-WORD%3E) to see the word cloud update in real time :rocket:
+
+:star2: Don't like the arrangement? [Regenerate it](https://github.com/trinib/word-cloud/issues/new?template=shufflecloud.md&title=wordcloud%7Cshuffle) :game_die:
+
+<div align="center">
+
+## #{CloudTypes::CLOUDPROMPTS.last}
+
+![Word Cloud Words Badge](https://img.shields.io/badge/Words%20in%20this%20Cloud-#{current_words_added}-informational?labelColor=003995)
+![Word Cloud Contributors Badge](https://img.shields.io/badge/Cloud%20Contributors-#{current_contributors.size}-blueviolet?labelColor=25004e)
+
+<img src="#{WORD_CLOUD_URL}" alt="WordCloud" width="100%">
+</div>
+
+   HTML
+    # TODO: [![Github Badge](https://img.shields.io/badge/-@username-24292e?style=flat&logo=Github&logoColor=white&link=https://github.com/username)](https://github.com/username)
+
+    current_contributors.each do |username, count|
+    end
+
+    markdown.concat()
+
+  end
+
+  private
+
+  def format_username(name)
+    name.gsub('-', '--')
+  end
+
+  def previous_cloud_url
+    url_end = CloudTypes::CLOUDPROMPTS[-2].gsub(' ', '-').gsub(':', '').gsub('?', '').downcase
+    "https://github.com/trinib/word-cloud/blob/main/previous_clouds/previous_clouds.md##{url_end}"
+  end
+
+  attr_reader :octokit
+end
